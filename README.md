3a1aef5d727973ca3e881d187812213c6948e4d7
sebi364 <110155948+sebi364@users.noreply.github.com>
2024-07-05 18:48:02 +0200

    minor fixes to pipelines + reorganized some stuff

---
+++ README.md
@@ -0,0 +1,3 @@
+https://www.kaggle.com/datasets/manann/quotes-500k
+https://www.rootisgod.com/2024/Running-an-HA-3-Node-K3S-Cluster/
+https://docs.google.com/document/d/1NGyn6Wito3yDE-4d_aPTKCUPobqt6S5PwaexumE2dk4/edit?usp=sharing
---
+++ webapp/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify
+import mariadb
+import os
+import uuid
+
+app = Flask(__name__)
+uuid = uuid.uuid4().hex
+
+@app.route('/api/quote', methods=['GET'])
+def submit():
+    try:
+        # create new connection
+        conn = mariadb.connect(
+            user=os.getenv('DB_USER', 'default'),
+            password=os.getenv('DB_PASSWORD', 'default'),
+            host=os.getenv('DB_HOST', 'mysqldb'),
+            port=3306,
+            database=os.getenv('DB_DATABASE', 'quotes')
+        )
+        # create new cursor, and querry DB for a random quote
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM quotes ORDER BY RAND() LIMIT 1;")
+        result = cur.fetchone()
+        # if result, return result
+        if result:
+            return jsonify({
+                "quote": result[1],
+                "author": result[2]
+            })
+        else:
+            return jsonify({})
+    except:
+        return jsonify("DB Error!"), 500
+
+@app.route('/health', methods=['GET'])
+def health():
+    return("ok", 200)
+
+@app.route('/uuid', methods=['GET'])
+def heaget_uuid():
+    return(uuid, 200)
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0')
