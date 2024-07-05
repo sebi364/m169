8d70d858e46e766c1345502867e80b9d21c47c76
sebi364 <110155948+sebi364@users.noreply.github.com>
2024-07-05 19:09:07 +0200

    removed scripts, because you can use kubectl apply -f . instead

--- K8s/apply.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-kubectl apply -f shared-secrets.yml
-kubectl apply -f shared-configs.yml
-kubectl apply -f mariadb-init-config.yml
-kubectl apply -f database.yml
-kubectl apply -f webapp.yml
-kubectl apply -f ingress.yml
